46548987bf0769e4ab5385bdcb71aa446d34fc7f
AY-CODED <ayomiposoyinka@gmail.com>
2026-04-25 09:09:23 +0100

    fix: resolve vite rollup error by importing Outlet from react-router-dom

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"; // Changed from "react-router"
+import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
